1ee1d397056b52ce0739d6ddb50c17b68903d303
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-08-24 22:52:32 -0400

    Server.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,12 +1,31 @@
-import requests, json  # Import the requests library to handle HTTP requests
+"""This module uses Watson AI Bert Sentiment from IBM to set the score of a given text."""
+# Import the requests library to handle HTTP requests
+import json
+import requests
+def sentiment_analyzer(text_to_analyse):
+    ''' Function named sentiment_analyzer that takes a string input (text_to_analyse) 
+    '''
+    # URL of the sentiment analysis service
+    url1 = 'https://sn-watson-sentiment-bert.labs.skills.network/'
+    url2 = 'v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    url = url1 + url2
 
-def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  # URL of the sentiment analysis service
-    myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    # Create a dictionary with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers required for the API request
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    # Send a POST request to the API with the text and headers
+    response = requests.post(url, json = myobj, headers=header, timeout=15)
+    label = ""
+    score = ""
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None
+    # Return the response text from the API
     res = {'label':label, 'score':score}
-    return res  # Return the response text from the API
+    return res
--- server.py
+++ server.py
@@ -2,16 +2,14 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
+# Import Flask, render_template, request from the flask pramework package:
 from flask import Flask, render_template, request
 
-app = Flask("Sentiment Analyzer")
-
-# Import the sentiment_analyzer function from the package created: TODO
+# Import the sentiment_analyzer function from the package created:
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
-
+#Initiate the flask app:
+app = Flask("Sentiment Analyzer")
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
@@ -19,15 +17,26 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    if text_to_analyze == "":
+        return "Please enter some text."
+    response = sentiment_analyzer(text_to_analyze)
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+    # Return a formatted string with the sentiment label and score
+    if label is None:
+        return "Invalid input, please try again."
+    res = f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
+    return res
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    #This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5500)
